0ecbb4d9c28d0596ed6e0984e450b047162a806a
Cody Kala <codykala@stanford.edu>
2018-05-15 01:47:51 -0700

    Adds skeleton files for data processing and models
    utils.py contains functions we use to get the data from the Kaggle and im2latex datasets. In the future, this file will contain other useful utility functions we implement as well.
    
    torch_model_base.py contains a skeleton for our PyTorch models. Ideally, all of the models we build should conform the API defined in torch_model_base.py. Currently, only "fit" and "predict" methods are defined.
    
    baseline_model.py contains a skeleton for our baseline model.

---
+++ baseline_model.py
@@ -0,0 +1,30 @@
+# Title: baseline_model.py
+# Authors: Cody Kala, Max Minichetti, David Lin
+# Date: 5/14/2018
+# ===========================
+# Defines a baseline model for our CS231n final project.
+
+import torch
+import torchvision
+import utils
+from torch_model_base import TorchModelBase
+
+class BaselineModel(TorchModelBase):
+
+	def __init__(self):
+		""" Stores the parameters that are relevant for the model """
+		raise Exception("Not yet implemented.")
+
+	def fit(self, X, y):
+		""" Trains the model on the training examples (X, y) """
+		raise Exception("Not yet implemented.")
+
+	def predict(X):
+		""" Predicts the labels for the examples in X """
+		raise Exception("Not yet implemented.")
+
+
+if __name__ == "__main__":
+	# Sanity checks for the model
+	raise Exception("Not yet implemented.")
+
