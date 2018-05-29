590352d7ee17aadd0ab7084e92931b8d2e8e8184
David <david.lin210@gmail.com>
2018-05-28 23:02:46 -0700

    skeleton files for encoder decoder translation model

---
+++ encoder.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+from torch.nn import init
+import torchvision
+import torchvision.transforms as T
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torch.utils.data import sampler
+import torchvision.datasets as dset
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+
+def build_encoder():
+return nn.Sequential(
+    
+)
