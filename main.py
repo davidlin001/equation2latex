6858e10abd1c0ce215d818d14bf519097ca131ba
Cody Keola Kala <codyakala@stanford.edu>
2018-05-31 11:10:29 -0700

    created main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+# Title: main.py
+# Author: Cody Kala
+# Date: 5/31/2018
+# =================
+# This module is where the functions in other modules are called in order
+# to train and develop our learning models.
+
+import torch
+import torchvision
+from model import *
+from train import *
+from predict import *
+from utils import *
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
