3240c2d82a9baeb38bce210b74dc8f615e8264e3
David <david.lin210@gmail.com>
2018-06-02 06:29:51 -0700

    modified based on guillaumegenthial

--- metrics.py
+++ metrics.py
@@ -1,67 +1,77 @@
 # Title: metrics.py
 # Author: David Lin
-# 
+#
+#
+# Based off of https://github.com/guillaumegenthial/im2latex/blob/master/model/evaluation/text.py 
 # ===================
 # This module defines scoring metrics to evaluate predictions
 
+import os
+import sys
+import numpy as np
 import distance
-from nltk.translate.bleu_score import sentence_bleu
+from nltk.translate.bleu_score import sentence_bleu, corpus_bleu
 
 def score(ref, hypo, method):
     """
-    
-    Params:
-    ref: Reference string
-    hypo:
-    method:
+    Args:
+        ref: List of all ground truth equation given as strings
+        hypo: List of all predicted equations given as strings
+        method: Select score metric
 
     Output:
-    score
+        score over all the strings for given method 
     """
     if method == "edit_distance":
         return edit_distance_score(ref,hypo)
     elif method == "bleu":
         return bleu_score(ref,hypo)
+    elif method == "exact":
+        return exact_match_score(ref,hypo)
     else:
         print("WARNING: Invalid method")
         return -1 
 
-
-def edit_distance_score(ref,hypo):
-    """
+def edit_distance_score(references, hypotheses):
+    """Computes Levenshtein distance between two sequences.
     Params:
-    ref: Correct String
-    hypo: Hypothesis String
-    
-    Output:
-    Edit/ Levenshtein Distance Score. Give the edit distance 
-    divided by max (len(ref), len(hypo))
+        references: List of ground truth equations
+        hypotheses: List of predicted equations
+    Returns:
+        1 is perfect. Gives the edit distance divided by max (len(ref), len(hypo))
     """
-    # To remove spaces if they are there
-    # ref = ref.replace(" ","")
-    # hypo = hypo.replace(" ", "")
-    percent_error = distance.nlevenshtein(ref,hypo,method=1)
-    return (1 - percent_error)
-    
-def bleu_score(ref, hypo):
-    """                                                                                    
-    Params:                                                                                 
-    ref: Correct String                                                                     
-    hypo: Hypothesis String                                                                  
-    Note: In order to leverage the bleu score, we treat each "gram" as a character in our math expression
+    edit_dist_tot, len_tot = 0, 0
+    for ref, hypo in zip(references, hypotheses):
+        edit_dist_tot += distance.levenshtein(ref, hypo)
+        len_tot += float(max(len(ref), len(hypo)))
 
-    Output:                                                                                  
-    bleu_score: 0 to 1 score. 1 is perfect. Checks for percentage of matches 
-    among 1,2,3, and 4-grams, giving equal weight to each. Must be extremely careful
-    of cases when the length of the equation is < 4 characters. 
+    return 1. - edit_dist_tot / len_tot
+
+def bleu_score(references, hypotheses):
+    """Computes bleu score.
+    Params:
+        references: list of ground truth equations
+        hypotheses: list of predicted equations
+    Returns:
+        bleu_score. Gives fraction of n-gram matches for n = 1,2,3,4
     """
-    # Be careful of the case 
-    assert len(ref),len(hypo) > 4
-    ref_list = list(ref)
-    hypo_list = list(hypo)
-    # Can tune how many n-grams to consider, right now considering up through 4-grams
-    return sentence_bleu([ref_list], hypo_list, weights=(0.25, 0.25, 0.25, 0.25))
-    #return sentence_bleu([ref_list], hypo_list, weights=(0.33,0.33,0.33,0))
+    # BE CAREFUL OF CASES where equation is less than length 4
+    # [ref] necessary for syntax 
+    references = [[ref] for ref in references] 
+    bleu_score = corpus_bleu(references, hypotheses, weights=(0.25, 0.25, 0.25, 0.25))
+    return bleu_score
 
-def exact_match_score(ref, hypo):
-    pass 
+def exact_match_score(references, hypotheses):
+    """Computes fraction of exact matches
+    Params:
+        references: list of ground truth equations
+        hypotheses: list of predicted equations
+    Returns:
+        fraction of exact matches
+    """
+    score = 0
+    for ref, hypo in zip(references, hypotheses):
+        if ref == hypo: score += 1
+    score /= len(hypotheses)
+    return score
+    
