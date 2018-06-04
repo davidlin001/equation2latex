2421b20b77c3ac1b00072c5da2097c992c16a85b
Cody Keola Kala <codyakala@stanford.edu>
2018-06-03 19:10:04 -0700

    minor change to main, added import statement for new data module I'm going to write

--- src/main.py
+++ src/main.py
@@ -7,18 +7,52 @@
 
 import torch
 import torchvision
+from data_utils import *
+from dataset import *
 from model import *
 from train import *
 from predict import *
 from utils import *
 
-#########################################################
-#### TODO: Implement basic UI functionality for main ####
-#########################################################
+# Filepaths to im2latex data
+images_path = "../data/full/images_processed"
+formulas_path = "../data/full/formulas.norm.lst"
+train_path = "../data/full/train_filter.lst"
+val_path = "../data/full/validate_filter.lst"
+test_path = "../data/full/test_filter.lst"
 
 def main():
-    raise NotImplementedError
+    # Load datasets
+    train_dataset = Im2LatexDataset(images_path, formulas_path, train_path)
+    val_dataset = Im2LatexDataset(images_path, formulas_path, val_path)
+    test_dataset = Im2LatexDataset(images_path, formulas_path, test_path)
 
+    # Initialize 
+    model = TranslationModel()
+    optim = torch.optim.Adam()
+    loss_fn = torch.nn.CrossEntropyLoss()
+    run_stats = {
+
+    # Run experiments
+    mode = input("mode? [train/test]")
+    checkpoint_path = input("path to checkpoint? Enter "" to use fresh model")
+    if mode == "train":
+        if checkpoint_path != "" 
+            state = load_checkpoint(checkpoint_path)
+            model.load_state_dict(state["model_state_dict"])
+            optim.load_state_dict(state["optim_state_dict"])
+            run_stats.update(state["run_stats"])
+            train_kwargs["start_epoch"] = state["start_epoch"]
+        model = train(model, optim, loss_fn, run_stats, train_dataset, 
+                        val_dataset, train_kwargs)
+    if mode == "test":
+        if checkpoint_path != ""
+            state = load_checkpoint(checkpoint_path)
+            model.load_state_dict(state["model_state_dict"])
+            optim.load_state_dict(state["optim_state_dict"])
+            run_stats.update(state["run_stats"])
+            train_kwargs["start_epoch"] = state["start_epoch"]
+    
 
 if __name__ == "__main__":
     main()
